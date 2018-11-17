75744b92587f10c90e6c4050cf0ae7f81b417cc5
lilggoode <44407639+lilggoode@users.noreply.github.com>
2018-11-16 19:41:32 -0500

    Uppercase Letters

--- Project2.s
+++ Project2.s
@@ -8,28 +8,19 @@
     main:
 
 	li $v0, 8   
-
 	la $a0, userInput
-
 	li $a1, 60
 	syscall
 	
 	removeLeadingSpace: 
 	li $t8, 32
 	lb $t9, 0($a0)
-
 	beq $t8, $t9, removeFirst
-
 	move $t9, $a0
-
 	j checkLength
 
-
-
 	removeFirst:
-
 	addi $a0, $a0, 1
-
 	j removeLeadingSpace
 	
 	checkLength:
@@ -115,6 +106,10 @@
 	
 	Numbers:
 	addi $s4, $s4, -48
+	j nextStep
+	
+	Upletters:
+	addi $s4, $s4, -55
 	j nextStep
 
 
