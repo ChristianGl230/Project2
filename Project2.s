8f2fe392833c941430b5148a3ba08e348a8344bf
lilggoode <44407639+lilggoode@users.noreply.github.com>
2018-11-16 19:04:52 -0500

    gets the user's input as text

--- Project2.s
+++ Project2.s
@@ -1,9 +1,15 @@
 .data
-
     emptyErrorMessage:	.asciiz "Input is empty."
-
     lengthErrorMessage: .asciiz "Input is too long."
-
     baseErrorMessage:   .asciiz "Invalid base-34 number."
-
     userInput:		.space 60
+.text
+
+    main:
+
+	li $v0, 8       #Obtain user's input as text 
+
+	la $a0, userInput
+
+	li $a1, 60
+	syscall
