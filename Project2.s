f1f034b392dbe6f0011803be10551a55a7f3f5a9
lilggoode <44407639+lilggoode@users.noreply.github.com>
2018-11-16 19:03:35 -0500

    the error messages

---
+++ Project2.s
@@ -0,0 +1,9 @@
+.data
+
+    emptyErrorMessage:	.asciiz "Input is empty."
+
+    lengthErrorMessage: .asciiz "Input is too long."
+
+    baseErrorMessage:   .asciiz "Invalid base-34 number."
+
+    userInput:		.space 60
